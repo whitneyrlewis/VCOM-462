d069f135b6c9ecef76153401ac218606a0bd5192
whitneyrlewis <lewisw11@mailbox.winthrop.edu>
2018-03-28 17:44:02 -0400

    first Attempt adding the backups for game

---
+++ let'sCookWorking/let'sCookWorking/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  let'sCookWorking
+//
+//  Created by WhitneyLewis on 3/13/18.
+//  Copyright © 2018 WhitneyLewis. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+        
+    @IBAction func startGame(_ sender: UIButton) {
+    }
+    
+    @IBAction func button2Recipes(_ sender: UIButton) {
+    }
+    
+    @IBAction func homeButton(_ sender: UIButton) {
+    }
+  
+    @IBAction func movePotatoe(_ sender: UIPanGestureRecognizer) {
+    }
+    @IBAction func movementPotatoe(_ sender: UITapGestureRecognizer) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
